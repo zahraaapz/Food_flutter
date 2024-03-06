b7aba3f031c76be48a2a135a28cf6a52af231c0f
zahraaapz <zahrapirooz1379@gmail.com>
2024-03-06 22:23:10 +0330

    add osm

--- lib/screen/des.dart
+++ lib/screen/des.dart
@@ -3,12 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
 import 'package:food_flutter/component/btn_style.dart';
 import 'package:food_flutter/component/color.dart';
+import 'package:food_flutter/component/text_style.dart';
 import 'package:food_flutter/widget/main_button.dart';
-
 import '../component/strings.dart';
+import '../component/api_key.dart';
 
 class Deliver extends StatefulWidget {
-
   Deliver({super.key});
 
   @override
@@ -16,21 +16,25 @@ class Deliver extends StatefulWidget {
 }
 
 class _DeliverState extends State<Deliver> {
-  int level=0;
-
+ List <GeoPoint> geoPoint=[];
   final controller =
       MapController(initPosition: GeoPoint(latitude: 35.7, longitude: 51.29));
-
+  final icon = Image.asset('assets/image/placeholder.png', scale: 10);
+  String address = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor:Colors.transparent,
+      backgroundColor: Colors.transparent,
       body: Stack(
         children: [
           OSMFlutter(
               controller: controller,
-              osmOption: const OSMOption(
-                zoomOption: ZoomOption(initZoom: 10),
+              osmOption: OSMOption(
+                markerOption: MarkerOption(
+                    advancedPickerMarker: MarkerIcon(
+                  iconWidget: icon,
+                )),
+                zoomOption: const ZoomOption(initZoom: 10),
                 isPicker: true,
               )),
           Positioned(
@@ -38,34 +42,100 @@ class _DeliverState extends State<Deliver> {
               top: 50,
               child: IconButton(
                 onPressed: () {
-                   setState((){
-                 level--;
-          });
+                  setState(() {
+                    if (geoPoint.isEmpty) {
+                      Navigator.of(context).pop();
+                    } else {
+                  
+                    address='';
+                    controller.removeMarker(geoPoint.last);
+                    geoPoint.remove(geoPoint.last);
+                    }
+                  
+                  });
+                  controller.advancedPositionPicker();
+                  
                 },
                 icon: const Icon(Icons.arrow_back),
                 style: BtnStyle.btnDes,
               )),
-      Positioned(
-        bottom: 10,
-        left:60,
-        child: MainButton(
-          onTap: () async{
-          await controller.getCurrentPositionAdvancedPositionPicker().then((value) => print(value));
-           controller.init();
-          
-          setState((){
-            level=1;
-          });
-          
-          },
-                size: const Size(400,800),
-                txtcolor: Colors.white,
-                txt:level==0?MyStrings.selectOrigin:level==1?MyStrings.selectDes:MyStrings.execute,
-                bgcolor: MyColor.bgButtonColor,
-        ),
-      ),  ],
+          destination()
+        ],
       ),
-   
+    );
+  }
+
+  execute() {
+    controller.zoomOut();
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return StatefulBuilder(
+          builder: (context,setState) => Container(
+            padding: const EdgeInsets.all(10),
+            height: 300,
+            width: double.infinity,
+            decoration: BoxDecoration(
+                color: Colors.white, borderRadius: BorderRadius.circular(20)),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Image.asset(
+                      'assets/image/destination.png',
+                      scale: 9,
+                    ),
+                    SizedBox(width: 230,child: Text(address,style: MyStyle.text,)),
+                  ],
+                ),
+                MainButton(
+                    size: const Size(400, 800),
+                    txtcolor: Colors.white,
+                    txt: MyStrings.execute,
+                    bgcolor: MyColor.bgButtonColor)
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Positioned destination() {
+    return Positioned(
+      bottom: 10,
+      left: 60,
+      child: MainButton(
+        onTap: () async {
+          await controller
+              .getCurrentPositionAdvancedPositionPicker()
+              .then((value) {
+            geoPoint.add(value);
+          });
+          controller.cancelAdvancedPositionPicker();
+
+          await controller.addMarker(geoPoint.first,
+              markerIcon: MarkerIcon(
+                iconWidget: icon,
+              ));
+           await getAddress(
+              lng: geoPoint.first.longitude, lat: geoPoint.first.latitude).then((value) {
+ setState(() {
+            address = value;
+          });
+              });
+         
+
+          execute();
+       
+        },
+        size: const Size(400, 800),
+        txtcolor: Colors.white,
+        txt: MyStrings.selectDes,
+        bgcolor: MyColor.bgButtonColor,
+      ),
     );
   }
 }
